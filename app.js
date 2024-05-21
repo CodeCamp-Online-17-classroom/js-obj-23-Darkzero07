95d901ba5ce243c2c168833795f0242ad482d217
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-21 07:17:20 +0700

    js lab 69

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// write code here
+class Calculator {
+    constructor(value) {
+        this.value = value
+    }
+    add(number) {
+        return this.value += number
+    }
+    subtract(number) {
+        return this.value -= number
+    }
+    multiply(number) {
+        return this.value *= number
+    }
+    divide(number) {
+        return this.value /= number
+    }
+    show() {
+        console.log(this.value)
+    }
+}
+
+let cal = new Calculator(10);
+console.log(cal.add(5))
+console.log(cal.subtract(3))
+console.log(cal.multiply(5))
+console.log(cal.divide(2))
+cal.show()
